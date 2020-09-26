a09f708940eb3bf923e711f0b711c490de3c7651
Mike Ritchie <patrick.ritchie89@gmail.com>
2020-09-26 14:07:50 -0400

    updated sections with information

--- _js/app.js
+++ _js/app.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
   // INJECT CSS
   var css = document.createElement('style');
-  css.type = 'text/css';
+  css.setAttribute('type', 'text/css');
   css.innerHTML = '#txt-rotate > .wrap { border-right: 0.08em solid #666 }';
   document.body.appendChild(css);
 
